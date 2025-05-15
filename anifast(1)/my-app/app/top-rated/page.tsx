96477d8712483ee0ba658557ddfaa94deb68f5de
rIMa2OO4 <97135130+abd2OO4@users.noreply.github.com>
2025-05-15 13:46:23 +0500

    Update toprated page

--- anifast(1)/my-app/app/top-rated/page.tsx
+++ anifast(1)/my-app/app/top-rated/page.tsx
@@ -83,7 +83,7 @@ useEffect(() => {
             <SearchBar />
           </div>
 
-          <h1 className="text-3xl font-bold mb-6">`Top Rated</h1>
+          <h1 className="text-3xl font-bold mb-6">Top Rated</h1>
 
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6">
             {animeList.map((anime) => (
